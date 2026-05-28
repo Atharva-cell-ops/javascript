9e8d8901de58d0440780e94b88a1e53e6c41940a
Atharva kale <atharvakale725@gmail.com>
2026-05-28 05:55:50 +0000

    container config

---
+++ 01_basics/operations.js
@@ -0,0 +1,15 @@
+let str1 = "Atharva"
+let str2 = "Kale"
+
+let str3 = str1 + str2
+console.log(str3);
+
+let gameprice1 = 1000
+const gameprice2 = ++gameprice1; //prefix operator
+console.log(gameprice1);
+console.log(gameprice2);
+
+let gameprice3 = 1000
+const gameprice4 = gameprice3++;  // postfix operator
+console.log(gameprice3);
+console.log(gameprice4);
