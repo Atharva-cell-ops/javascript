0f908bf9b203fd9a75fb71a7c0ce1cd9b4f876be
Atharva kale <atharvakale725@gmail.com>
2026-06-10 11:27:54 +0000

    container config

---
+++ 01_basics/array.js
@@ -0,0 +1,8 @@
+// array
+
+const myarr = [0,1,2,3,4,5]
+const myheroes = ["spiderman, captain america,ironman"]
+myarr.push(7)
+console.log(myarr);
+
+console.log(myarr.includes(4));
---
+++ 01_basics/objects.js
@@ -0,0 +1,53 @@
+// singleton 
+
+// object literals
+
+// const Jsuser = {
+//     name: "Atharva",
+//     age: "18",
+//     location:"Nagpur",
+//     email:"atharvakale725@gmail.com",
+//     isLoggedIn: false,
+//     LastLoginDays: ["Monday","Tuesday"]
+// }
+
+// console.log(Jsuser.email)
+// console.log(Jsuser["email"])
+
+// Jsuser.email = "atharva@gogglekaggle.com"
+// Object.freeze(Jsuser)
+// Jsuser.email = "atharva@microsoft.com"
+// console.log(Jsuser);
+
+// Jsuser.greeting = function(){
+//     console.log("Hello Js user");
+// }
+
+// console.log(Jsuser.greeting);
+
+// const tinderuser = new object()
+
+const tinderuser = {}
+
+tinderuser.id = "123abc"
+tinderuser.name = "Sam"
+tinderuser.isloggedIn = false
+
+console.log(tinderuser);
+
+const regularuser = {
+    email:"xyz@gmail.com",
+    fullname:{
+        username:{
+            firstname:"Atharva",
+            lastname:"Kale"
+        }
+    }
+}
+
+const obj = {1:"a",2 : "b"}
+const obj2 = {3:"a",2:"b"}
+
+// const obj3 = {obj, obj2}
+const obj3 = Object.assign({},obj,obj2)
+console.log(obj3);
