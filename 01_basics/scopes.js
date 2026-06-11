be21add7cf3dae81d6b3cedf8d01ae9672e2712c
Atharva kale <atharvakale725@gmail.com>
2026-06-11 11:52:44 +0000

    container config

--- 01_basics/scopes.js
+++ 01_basics/scopes.js
@@ -1,13 +1,36 @@
-let a = 20
-const b  = 30
-var c = 40
+// let a = 20
+// const b  = 30
+// var c = 40
 
-a = 30000
-if(true){
-    let a = 10
-    const b = 20
-    console.log("INNER",a);
+// a = 30000
+// if(true){
+//     let a = 10
+//     const b = 20
+//     console.log("INNER",a);
+// }
+// console.log(c)
+// console.log(b)
+// console.log(a)
+
+function one(){
+    const username = "Atharva"
+
+    function two(){
+        const website = "Goggle"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+
+    if(true){
+        const username = "Atharva"
+        if(username==="Atharva"){
+            const website = "Insta"
+            console.log(username + website);
+        }
+    }
 }
-console.log(c)
-console.log(b)
-console.log(a)
+
+one()
+// console.log(username)
