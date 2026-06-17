30d357a1fcf3c91b317e4c62cd4cebc0f4796916
Atharva kale <atharvakale725@gmail.com>
2026-06-17 06:54:52 +0000

    container config

--- 01_basics/constructorFunction.js
+++ 01_basics/constructorFunction.js
@@ -4,7 +4,22 @@ function BellBoy(name, age, hasworkpermit, languages, yearsofexperience){
     this.hasworkpermit = hasworkpermit;
     this.languages = languages;
     this.yearsofexperience = yearsofexperience;
+
+    this.greet = function() {
+        console.log(`Hello, my name is ${this.name}. I am ${this.age} years old.`);
+        this.pickUpLuggage();
+        this.move();
+    };
 }
 
-let bellBoy = new BellBoy("Timmy", 50, true, ["French", "Indian", "English"], 20);
-console.log(bellBoy);
+BellBoy.prototype.pickUpLuggage = function() {
+    console.log(`${this.name} is picking up luggage.`);
+};
+
+BellBoy.prototype.move = function() {
+    console.log(`${this.name} is moving luggage to the room.`);
+};
+
+let bellboy1 = new BellBoy("John", 25, true, ["English", "Spanish"], 3);
+bellboy1.greet();
+
